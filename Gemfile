fa576001b0728e534f40711a5c3da85d3bb9e9c0
Dyson Sumsion <Dyson@MacBook-Air-2.local>
2019-07-29 17:05:21 +1000

    added calendar to listings

--- Gemfile
+++ Gemfile
@@ -4,6 +4,12 @@ git_source(:github) { |repo| "https://github.com/#{repo}.git" }
 ruby '2.5.5'
 gem "aws-sdk-s3", require: false
 gem 'devise'
+
+
+gem 'hirb'
+
+# Calendar for all booked listings
+gem "simple_calendar", "~> 2.0"
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '~> 5.2.3'
 # Use postgresql as the database for Active Record
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_07_29_021200) do
+ActiveRecord::Schema.define(version: 2019_07_29_053900) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -37,8 +37,8 @@ ActiveRecord::Schema.define(version: 2019_07_29_021200) do
   end
 
   create_table "listings", force: :cascade do |t|
-    t.date "date_from"
-    t.date "date_to"
+    t.date "start_time"
+    t.date "end_time"
     t.integer "price"
     t.bigint "motorhome_id"
     t.datetime "created_at", null: false
