458ecf98d5eca393e37be8591aeed52ffef6c0a2
Farts-Jousta <49703765+Farts-Jousta@users.noreply.github.com>
2019-07-30 14:14:01 +1000

    added search-bar to listings page

--- app/controllers/listings_controller.rb
+++ app/controllers/listings_controller.rb
@@ -4,7 +4,23 @@ class ListingsController < ApplicationController
   # GET /listings
   # GET /listings.json
   def index
-    @listings = Listing.all
+    @listing_date_from = Listing.all
+    @listing_date_to = Listing.all
+    @listing_max_price = Listing.all
+    @listing_min_beds = Listing.all
+    if params[:date_from] != nil && params[:date_from] != ''
+      @listing_date_from = Listing.where("start_time <= '#{Date.parse(params[:date_from])}'")
+    end
+    if params[:date_to] != nil && params[:date_to] != ''
+      @listing_date_to = Listing.where("end_time >= '#{Date.parse(params[:date_to])}'")
+    end
+    if params[:max_price] != nil && params[:max_price] != ''
+      @listing_max_price = Listing.where("price < '#{params[:max_price]}'")
+    end
+    if params[:min_beds] != nil && params[:min_beds] != ''
+      @listing_min_beds = Listing.joins(:motorhome).where("motorhomes.beds >= '#{params[:min_beds]}'")
+    end
+    @listings = @listing_date_from & @listing_date_to & @listing_max_price & @listing_min_beds
   end
 
   # GET /listings/1
