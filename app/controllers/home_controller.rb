c25c12c41ddab0a53e4f8d124ed5b2fe3ae606af
Dyson Sumsion <Dyson@MacBook-Air-2.local>
2019-08-12 11:47:35 +1000

    stripe

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,35 +1,25 @@
 class HomeController < ApplicationController
 
-    def index
-        if user_signed_in?
-            @user_motorhomes = Motorhome.where(user: current_user)
-        end
-
-        @listing_date_from = Listing.all
-        @listing_date_to = Listing.all
-        @listing_max_price = Listing.all
-        @listing_min_beds = Listing.all
-        @listing_search = Listing.all
-        def index
-          if user_signed_in?
-              @user_motorhomes = Motorhome.where(user: current_user)
-          end
-          @listing_date_from = Listing.all
-          @listing_date_to = Listing.all
-          @listing_max_price = Listing.all
-          @listing_search = Listing.all
-          if params[:search] != nil && params[:search] != ''
-            @listing_search = Listing.where('listings.address LIKE ?', "%#{params[:search]}%")
-          end
-          if params[:start_time] != nil && params[:start_time] != ''
-            @listing_date_from = Listing.where("start_time <= '#{Date.parse(params[:start_time])}'")
-          end
-          if params[:end_time] != nil && params[:end_time] != ''
-            @listing_date_to = Listing.where("end_time >= '#{Date.parse(params[:end_time])}'")
-          end
-          if params[:max_price] != nil && params[:max_price] != ''
-            @listing_max_price = Listing.where("price < '#{params[:max_price]}'")
-          end
-          @listings = @listing_search & @listing_date_from & @listing_date_to & @listing_max_price 
-      end
+  def index
+    if user_signed_in?
+        @user_motorhomes = Motorhome.where(user: current_user)
+    end
+    @listing_date_from = Listing.all
+    @listing_date_to = Listing.all
+    @listing_max_price = Listing.all
+    @listing_search = Listing.all
+    if params[:search] != nil && params[:search] != ''
+      @listing_search = Listing.where('listings.address LIKE ?', "%#{params[:search]}%")
+    end
+    if params[:start_time] != nil && params[:start_time] != ''
+      @listing_date_from = Listing.where("start_time <= '#{Date.parse(params[:start_time])}'")
+    end
+    if params[:end_time] != nil && params[:end_time] != ''
+      @listing_date_to = Listing.where("end_time >= '#{Date.parse(params[:end_time])}'")
+    end
+    if params[:max_price] != nil && params[:max_price] != ''
+      @listing_max_price = Listing.where("price < '#{params[:max_price]}'")
+    end
+    @listings = @listing_search & @listing_date_from & @listing_date_to & @listing_max_price 
+  end
 end
